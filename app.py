b8452c4cd675d01115018d767780eae188c600fb
Sebastian Lopez-Marcano <slopezmarcano@gmail.com>
2023-02-13 11:33:36 +1000

    v1.7 enhancements #5
    1. Ecological metrics checklist as per #5
    2. Included more oceans and seas in the location
    3. Adjusted the options in the species dropdown list so its now more descriptive
    4. Improved the layout of the app
    5. Increased the project reduction per sampling period to 40%
    6. Improved the description in the instructions, as prices are not realistic
    7. Added more options for no_videos slider
    8. Fixed the font of the generate_linegraph function output

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pandas as pd
 import dash_daq as daq
 
 # -- APP VERSION --#
-app_version ="v1.6.1"
+app_version ="v1.7"
 
 version = html.Div([
     html.Div(f"SLM - Version: {app_version}",
@@ -20,7 +20,7 @@ version = html.Div([
 mytitle = dcc.Markdown(children='# The FishID Budget App') #Header or title of the page
 
 #Instructions
-instructions = dcc.Markdown('Hi! Welcome to the FishID Budget App. This app will provide a rough estimation of the investment required to develop a FishID model. Please complete the questions and click submit for the app to display the FishID budget. Default values are displayed on the right before user clicks submit')
+instructions = dcc.Markdown('Hi! Welcome to the FishID Budget App. This app will provide a rough estimation of the investment required to develop a FishID model. Please complete the questions and click submit for the app to display the FishID budget. Default values are displayed on the right before user clicks submit. Prices are not realistic and are displayed just for drafting purposes.')
 
 
 #-- SET THEME --#
@@ -63,7 +63,11 @@ column1 = dbc.Col (
                                             {"label": "Atlantic Ocean", "value": 4},
                                             {"label": "Indian Ocean", "value": 5},
                                             {"label": "Red Sea", "value": 6},
-                                            {"label": "Baltic Sea", "value": 7},],value=0),
+                                            {"label": "Baltic Sea", "value": 7},
+                                            {"label": "Caspian Sea", "value": 8},
+                                            {"label": "Black Sea", "value": 9},
+                                            {"label": "Artic Ocean", "value": 10},
+                                            {"label": "Southern Ocean", "value": 11},],value=0),
             #dcc.Graph(id="map", figure=location_map)
              ],
             className="mb-3"
@@ -76,13 +80,17 @@ column1 = dbc.Col (
         #species_type_dropdown
         html.Div(
             [dbc.Label("Select the option that best fit the species type", html_for="dropdown", style= {'font-weight' : 'bold'}),
-            dcc.Dropdown(id="species-type", options=[{"label": "Commercial", "value": "commercial"},{"label": "Abundant", "value": "abundant"},
-            {"label": "Keystone", "value": "keystone"},{"label": "Rare and Non-Abundant", "value": "rare"}],value='commercial'),], className="mb-3"),
+            dcc.Dropdown(id="species-type", options=[{"label": "Commercial and Abundant ", "value": "commercial"},
+                                                    {"label": "Non-Commercial and Abundant", "value": "abundant"},
+                                                    {"label": "Non-Commercial and Non-Abundant", "value": "rare"},
+                                                    {"label": "Keystone / Important and Abundant", "value": "keystone"},
+                                                    {"label": "Keystone / Important and Non Abundant", "value": "rare"},
+                                                    {"label": "Invasive / Rare and Non-Abundant", "value": "rare"}],value='commercial'),], className="mb-3"),
         
         #no_videos_slider
         html.Div(
             [dbc.Label("# of hours FishID will process", html_for="slider", style= {'font-weight' : 'bold'}),
-            dcc.Slider(id="video-count", min=100, max=1000, step=250, value=100),], className="mb-3"),
+            dcc.Slider(id="video-count", min=100, max=10000, step=1000, value=100),], className="mb-3"),
         
         #no_of habitats
         html.Div(
@@ -96,24 +104,39 @@ column1 = dbc.Col (
                                                             {"label": "Rocky Reef", "value": 2}, {"label": "Kelp Forest", "value": 3}, {"label": "Estuaries", "value": 4},
                                                             {"label": "Deep Sea", "value": 5}, {"label": "Aerial", "value": 6}, {"label": "Unstructured habitat", "value": 7} ], value = 0),], className = "mb-3"),
 
-        #annotation support
+        
+    ],
+    md=6,
+)
+#Column2
+column2 = dbc.Col(
+    [
+         #annotation support
         html.Div(
-            [dbc.Label("Do you require FishID's annotation support ?"),
-            daq.BooleanSwitch(id="anno-switch", on = True, color="#388697",)]),
+            [dbc.Label("Do you require FishID's annotation support ?", style= {'font-weight' : 'bold'}),
+            daq.BooleanSwitch(id="anno-switch", on = True, color="#388697",)]), 
 
         #development timeframe
         html.Div(
             [dbc.Label("Timeframe (months) for model development", html_for="slider", style= {'font-weight' : 'bold'}),
             dcc.Slider(id="dev-time", min=2, max=10, step=1, value=3),], className="mb-3"),
 
+
+        #ecoloigcal metrics
+        html.Div(
+            [dbc.Label("Ecological metrics to be extracted", html_for="checklist", style= {'font-weight' : 'bold'}),
+            dcc.Checklist(id='eco-metrics', options=['MaxN', 'MeanN', 'T1', 'Tracking Behaviour', 'Sizing'],
+            value=['MaxN', 'MeanN', "T1"],
+            inline = True),], className="mb-3"),
+
         #submit_button   
         html.Div(
-            [dbc.Button("Submit", id="submit-button", color="primary", className ="btn-success"),], className="mt-3"),       
+            [dbc.Button("Submit", id="submit-button", color="primary", className ="btn-success"),], className="mt-3")
     ],
-    md=4,
-)
-#Column2
-column2 = dbc.Col(
+    md=6
+    )
+#Column3
+column3 = dbc.Col(
     [
         html.H3("Project Investment", className="text-center mt-3"),
         html.H6(id="output1", className="text-center text-success"),
@@ -128,6 +151,7 @@ app.layout = dbc.Container(
         mytitle,
         instructions, 
         dbc.Row([column1, column2]),
+        dbc.Row([column3]),
         version
     ],
     fluid=True,
@@ -150,24 +174,25 @@ app.layout = dbc.Container(
         dash.dependencies.State("no-habitats", "value"),
         dash.dependencies.State("habitat-type", "value"),
         dash.dependencies.State("dev-time", "value"),
+        dash.dependencies.State("eco-metrics", "value")
     ],
 )
 
 
-def budget_calculation(on, n_clicks, species_count, species_location, species_type, video_count, no_habitats, habitat_type, dev_time):
+def budget_calculation(on, n_clicks, species_count, species_location, species_type, video_count, no_habitats, habitat_type, dev_time, eco_metrics): #TODO #7
     
     if on: 
-        if species_location == 0 and (species_type == 'commercial' or species_type =='abundant') and habitat_type == 0 and dev_time ==2: #easy-case scenario but with short dev time
+        if species_location == 0 and (species_type == 'commercial' or species_type =='abundant') and habitat_type == 0 and (dev_time ==2 or dev_time ==3): #easy-case scenario but with short dev time
             annos = 500 #for each species commercial species where there is a lot of data
-            dev_cost = 18000 #lower dev time = higher cost . 2 RA for 2 months
-
+            dev_cost = 20000 #lower dev time = higher cost . 2 RA for 2 months
+        
             species_cost = species_count * annos * no_habitats
             video_cost = video_count * 50 #processing cost 
             total_cost = species_cost + video_cost + dev_cost
         else:
             annos = 3000 #for each species for species outside FID region
             habitat = 5000 #challening habitats fee
-            dev_cost = 6000 * dev_time #standard fee for each month of development
+            dev_cost = 4000 * dev_time #standard fee for each month of development
             
             species_cost = species_count * annos + habitat * no_habitats
             video_cost = video_count * 50 #processing cost 
@@ -189,15 +214,15 @@ def budget_calculation(on, n_clicks, species_count, species_location, species_ty
 
 def generate_linegraph(total_cost):
     sample_period = range(1, 8)
-    output_value = [max(total_cost * (1- 0.3 * (i)), 1000) for i in range(7)]
+    output_value = [max(total_cost * (1- 0.4 * (i)), 1000) for i in range(7)]
     line_graph = px.line(x=sample_period, y=output_value, labels={'x': 'Sampling Period', 'y': 'Processing and Model Investment in AU$'})
     line_graph.update_layout(
-        title={'text': "30 percent cost reduction for each sampling period", 'font': {'size': 14}},
-        xaxis_title="",
-        yaxis_title="",
+        title={'text': "40 percent cost reduction for each sampling period", 'font': {'size': 14}},
+        xaxis_title="Sampling period",
+        yaxis_title="AU Price",
         xaxis=dict(showgrid=False, zeroline=False),
-        yaxis=dict(showgrid=False, zeroline=False),
-        font=dict(family='Courier New, monospace', size=12, color='black')
+        yaxis=dict(showgrid=False, zeroline=False)#,
+        #font=dict(family='Courier New, monospace', size=12, color='black')
     )
     return (line_graph)
 
